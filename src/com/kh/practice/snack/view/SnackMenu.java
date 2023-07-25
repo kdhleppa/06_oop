682f9a3bc46741cdef804c4be95170e32853f2bb
kdhleppa <newkdh8624@gmail.com>
2023-07-25 17:51:49 +0900

    final

--- src/com/kh/practice/snack/view/SnackMenu.java
+++ src/com/kh/practice/snack/view/SnackMenu.java
@@ -1,5 +1,40 @@
 package com.kh.practice.snack.view;
 
+import java.util.Scanner;
+
+import com.kh.practice.snack.controller.SnackController;
+
 public class SnackMenu {
+	
+	public void menu() {
+		
+	
+		Scanner sc = new Scanner(System.in);
+		SnackController scr = new SnackController();
+	
+	
+		System.out.println("스낵류를 입력하세요.");
+		System.out.print("종류 : ");
+		String inputKind = sc.next();
+	
+		System.out.print("이름 : ");
+		String inputName = sc.next();
+	
+		System.out.print("맛 : ");
+		String inputFlavor = sc.next();
+			
+		System.out.print("개수 : ");
+		int inputNumOf = sc.nextInt();
+	
+		System.out.print("가격 : ");
+		int inputPrice = sc.nextInt();
+	
+	
+	
+	
+	
+	
+		
+	}			
 
 }
